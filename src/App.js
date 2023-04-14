eb9384ecbf7a0cb2f2073d890747802638071ba9
GarvitKaushik123 <gkaushik@iitg.ac.in>
2023-04-14 12:42:15 +0530

    toggle feature added

--- src/App.js
+++ src/App.js
@@ -8,9 +8,19 @@ import Portfolio from "./components/Portfolio/Portfoliio";
 import Testimonial from "./components/Testimonial/Testimonial";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import {themeContext} from './Context';
+import { useContext } from "react";
+
 function App() {
+  const theme = useContext(themeContext)
+  const darkMode = theme.state.darkMode
   return (
-    <div className="App">
+    <div className="App"
+      style={{
+        background : darkMode?'black':'',
+        color: darkMode?'white':'',
+      }}
+    >
       <Navbar/>
       <Intro/>
       <Services/>
